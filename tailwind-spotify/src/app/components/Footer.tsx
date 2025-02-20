6579ba37286d2042af2d87d7be44bf4eeab57c97
brissowkevin <kevinbrissowdev@gmail.com>
2025-02-20 14:53:03 -0300

    teste

--- tailwind-spotify/src/app/components/Footer.tsx
+++ tailwind-spotify/src/app/components/Footer.tsx
@@ -40,7 +40,7 @@ export function Footer() {
         <Volume size={20}/>
         <div className='h-1 rounded-full w-24 bg-zinc-600'>
             <div className='bg-zinc-200 w-10 h-1 rounded-full'></div>
-          </div>
+        </div>
       </div>
       <Maximize2 size={20}/>
     </div>
